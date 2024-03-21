dcbe8adaf156a09cd1a916c5bcf8e264621ad8bf
Richa Arora <richaaeri9988@gmail.com>
2024-03-21 12:28:31 +0530

    content file

--- src/Components/ContentRating.jsx
+++ src/Components/ContentRating.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
-import './ContentRating.css'
-export class ContentRating extends Component {
+// ImageRatingComponent.js
+
+import React, { Component } from 'react';
+import './ContentRating.css';
+
+class ContentRating extends Component {
+  constructor() {
+    super();
+  }
   render() {
     return (
      <>
      <h1>Text Content Rating</h1>
      </>
-    )
+    );
   }
 }
 
-export default ContentRating
+export default ContentRating;
